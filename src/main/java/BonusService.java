fb2c7325fbd1187116d6fc28c3f386e0a049a2e3
ValeryGil <valery.gil@icloud.com>
2023-05-02 15:52:54 +0300

    Fix #1

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,12 +1,12 @@
 public class BonusService {
 
     public long calculate(long amount, boolean registered) {
-        int Percent = registered ? 3 : 1;
+        int percent = registered ? 3 : 1;
         long bonus = amount * percent / 100;
         long limit = 500;
         if (bonus > limit) {
             bonus = limit;
         }
-        return bonus;
+        return bonus;git statusgii
     }
 }
